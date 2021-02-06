5e8b786e621f85715589c3d8a61f933bdc02fa3e
Giuliano Scollo <g.scollo@tu-bs.de>
2021-02-06 21:08:08 +0100

    changed site to change to in js

--- login_page.js
+++ login_page.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (uName == "admin" && passW == "12345") {
         alert("You have successfully logged in.");
-        var newUrl = "127.0.0.1:550/dashboard.html"
+        var newUrl = "https://www.giuisco.github.io/Organizer/dashboard.html/"
         window.open(newUrl);
     } else {
         loginErrorMsg.style.opacity = 1;
